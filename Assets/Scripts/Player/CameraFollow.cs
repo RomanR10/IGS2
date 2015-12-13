ba0df7a34b030c2dd2249ab2793558a3970dfc4b
Derek Shunia <eventhorizonsoundtracks@gmail.com>
2015-12-13 18:24:22 -0500

    Added basic movement and camera follow, and a basic procedural generation script

---
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+    public bool autoFindPlayer; //Finds GameObject tagged "Player" if enabled
+    public bool followVertically; //Should the camera move along the Y axis with the player?
+    public GameObject target; //Used if AutoFind is disabled
+    public float followDistance;
+    public float heightAbovePlayer;
+    private GameObject player;
+    private Rigidbody body;
+
+    void Start()
+    {
+        if (autoFindPlayer)
+            player = GameObject.FindGameObjectWithTag("Player");
+        else
+            player = target;
+
+        if (player == null)
+            Debug.LogError("Camera has nothing to follow. Did you forget to assign the camera a follow target?");
+        else
+            body = player.GetComponent<Rigidbody>();
+
+        if (body == null)
+            Debug.LogError("Target has no RigidBody attached to it.");
+    }
+
+    void Update()
+    {
+        if (body != null)
+            UpdateCameraPosition();
+    }
+
+    void UpdateCameraPosition()
+    {
+        //Use camera's Y position if FollowVertically is disabled
+        Vector3 targetPosition = followVertically ? player.transform.position 
+            : new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
+        Vector3 destination = targetPosition + (Vector3.back * followDistance) + (Vector3.up * heightAbovePlayer);
+        transform.position = destination;
+    }
+}
---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+    public float maxSpeed;
+    private Rigidbody player;
+
+    void Start()
+    {
+        player = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        player.velocity = Vector3.right * Input.GetAxis("Horizontal") * maxSpeed;
+    }
+}
